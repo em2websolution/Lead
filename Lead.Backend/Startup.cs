f7899d364dc02686f558f6b7c4d96422d707f171
Eduardo Marques <psyfreak2012@gmail.com>
2022-11-26 17:20:27 -0300

    update swagger

--- Lead.Backend/Startup.cs
+++ Lead.Backend/Startup.cs
@@ -41,16 +41,13 @@ namespace Lead.Backend
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lead.Backend API");
-                    c.RoutePrefix = string.Empty;
-                });
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lead.Backend API");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
